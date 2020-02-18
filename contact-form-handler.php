d91f252f1523447b0ed2cc0f993a92f62e5a2582
Henri <60843202+NordlichtLP@users.noreply.github.com>
2020-02-18 22:17:54 +0100

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+  $subject = $_POST ['betreff'];
+  $mailFrom = $_POST['mail'];
+  $message = $_POST['message'];
+
+  $mailTo = "creeper07hd@gmail.com";
+  $headers = "From: ".$mailFrom;
+  $txt = ".\n\n".$message;
+
+
+  mail($mailTo, $subject, $txt, $headers);
+}
+?>
